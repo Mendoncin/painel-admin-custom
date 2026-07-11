b0e34a850eb0fbfc48ffd898ea529edeba340943
Mendoncin <gabriel.cabecao.lol@gmail.com>
2026-07-11 16:18:28 -0300

    endpoint basico

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+import datetime
 
 # Create your views here.
+
+def abigail(request: HttpRequest) -> HttpResponse:
+  now = datetime.datetime.now()
+  return HttpResponse("<html>"
+                      "<h1>Abigail!</h1>"
+                      f"<h2>Agora é: {now}</h2>"
+                      "</html>"
+                      )
--- mate/urls.py
+++ mate/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from catalog.views import abigail
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("abigail/", abigail)
 ]
