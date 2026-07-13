c7f945fe7c0e7c6b78e732c55e1667ac08e0e54a
Mendoncin <gabriel.cabecao.lol@gmail.com>
2026-07-13 17:08:42 -0300

    html estatico no template

--- catalog/views.py
+++ catalog/views.py
@@ -16,12 +16,4 @@ def abigail(request: HttpRequest, unique_number: int) -> HttpResponse:
 def index(request: HttpRequest) -> HttpResponse:
   num_books = Book.objects.count()
   num_author = Author.objects.count()
-  return HttpResponse(
-    "<html>"
-    "<h1>HOME PAGE</h1>"
-    "<ul>"
-    f"<li>Número de Livros no catálogo: {num_books}</li>"
-    f"<li>Número de Autores no catálogo: {num_author}</li>"
-    "</ul>"
-    "</html>"
-  )
+  return render(request, "catalog/index.html")
