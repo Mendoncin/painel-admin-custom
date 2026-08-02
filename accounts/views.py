d3ad6c6d213a49b7a8223e3ad333d2880ca19210
Mendoncin <gabriel.cabecao.lol@gmail.com>
2026-08-02 14:52:02 -0300

    login logica manual

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 # Create your views here.
 
 def login_view(request: HttpRequest) -> HttpResponse:
-  return render(request, "accounts/login.html")
+  if request.method == "GET":
+    return render(request, "accounts/login.html")
+  elif request.method == "POST":
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(username=username, password=password)
+    if user:
+      login(request, user)
+      return HttpResponseRedirect(reverse("catalog:index"))
+    return render(request, "accounts/login.html")
